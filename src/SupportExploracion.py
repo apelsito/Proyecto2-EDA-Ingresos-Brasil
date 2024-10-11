ddf509761840c679ea00189b962084242ff12c7c
apelsito <mrappletastico@gmail.com>
2024-10-11 19:48:15 +0200

    Tenemos datos tabla y columnas arregladas

---
+++ src/SupportExploracion.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import warnings
+warnings.filterwarnings('ignore')
+
+def traducir(column):
+   
+    traductor = {
+    'CÓDIGO ÓRGÃO SUPERIOR': 'código del órgano superior',
+    'NOME ÓRGÃO SUPERIOR': 'nombre del órgano superior',
+    'CÓDIGO ÓRGÃO': 'código del órgano',
+    'NOME ÓRGÃO': 'nombre del órgano',
+    'CÓDIGO UNIDADE GESTORA': 'código de la unidad gestora',
+    'NOME UNIDADE GESTORA': 'nombre de la unidad gestora',
+    'CATEGORIA ECONÔMICA': 'categoría económica',
+    'ORIGEM RECEITA': 'origen de los ingresos',
+    'ESPÉCIE RECEITA': 'tipo de ingresos',
+    'DETALHAMENTO': 'detalle',
+    'VALOR PREVISTO ATUALIZADO': 'valor previsto actualizado',
+    'VALOR LANÇADO': 'valor registrado',
+    'VALOR REALIZADO': 'valor ejecutado',
+    'PERCENTUAL REALIZADO': 'porcentaje ejecutado',
+    'DATA LANÇAMENTO': 'fecha de registro',
+    'ANO EXERCÍCIO': 'año fiscal'
+    }
+    traducida = traductor[column]
+    rename_columna = {column : traducida}
+    return rename_columna
+
+def fill_year(df,year):
+    df["año fiscal"].replace(np.nan,year,inplace=True)
+    return
+
